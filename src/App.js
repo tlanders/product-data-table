d468384e10bce64db512ea030de8f2fd579b7260
tlanders <768713+tlanders@users.noreply.github.com>
2020-01-05 16:37:29 -0600

    added static html and some css

--- src/App.js
+++ src/App.js
@@ -5,20 +5,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form id="search" name="search">
+        <input type="text" value="" name="searchText"/><br/>
+          <input type="checkbox" name="inStockOnly"/> Only show products in stock
+      </form>
+        <div id="product-table">
+        <table>
+            <thead>
+            <th>Name</th>
+            <th>Price</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><b>Sporting Goods</b></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Football</td>
+                    <td>$49.99</td>
+                </tr>
+                <tr>
+                    <td><b>Electronics</b></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>iPod Touch</td>
+                    <td>$9.99</td>
+                </tr>
+                <tr>
+                    <td class="not-stocked">iPhone 5</td>
+                    <td>$49.99</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
     </div>
   );
 }
