7ac9c4b73a845ff10af3cc833a65bb86859fffc0
tlanders <768713+tlanders@users.noreply.github.com>
2020-01-05 17:50:42 -0600

    put state in top component added onchange handlers to search inputs

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,10 +3,36 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
+        this.state = {
+            products: props.products,
+            inStockOnly: false,
+            searchText: ''
+        };
+    }
+
+    handleSearchTextChange(text) {
+        this.setState({
+            searchText: text
+        });
+    }
+
+    handleInStockChange(inStock) {
+        this.setState({
+            inStockOnly: inStock
+        });
+    }
+
     render() {
         return (
             <div id="filterable-table">
-                <SearchBar/>
+                <SearchBar
+                    onSearchTextChange={this.handleSearchTextChange}
+                    onInStockChange={this.handleInStockChange}
+                />
                 <ProductTable products={this.props.products}/>
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.onSearchTextChange = this.onSearchTextChange.bind(this);
+        this.onInStockChange = this.onInStockChange.bind(this);
+    }
+
+    onSearchTextChange(evt) {
+        this.props.onSearchTextChange(evt.target.value);
+    }
+
+    onInStockChange(evt) {
+        this.props.onInStockChange(evt.target.value);
+    }
+
     render() {
         return (
             <div>
                 <form id="search-bar" name="search-bar">
                     <p>
-                    <input type="text" value="" name="searchText" placeholder="Search..."/><br/>
-                    <input type="checkbox" name="inStockOnly"/> Only show products in stock
+                    <input
+                        type="text"
+                        value={this.props.searchText}
+                        onChange={this.onSearchTextChange}
+                        placeholder="Search..."
+                    /><br/>
+                    <input
+                        type="checkbox"
+                        onChange={this.onInStockChange}
+                    >{this.props.inStockOnly}</input> Only show products in stock
                     </p>
                 </form>
             </div>
